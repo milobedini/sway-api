f7e2451440e4eb63bca3560eb2d6f28f7a17cd88
milobedini <milobedini64@gmail.com>
2023-01-27 18:06:42 +0000

    environ variables

--- sway/settings.py
+++ sway/settings.py
@@ -24,18 +24,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = '3j@-nsd*9-!&q7sk1p5*vmz215*5owy6zwq6$1log)r)@%spo9'
+
 dotenv_file = os.path.join(BASE_DIR, '.env')
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = os.environ['DEBUG']
-# DEBUG = True
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
